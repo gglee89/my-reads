afddf7a755c6ac10714d85d4bed1785191b1a898
Giwoo Lee <gglee89@gmail.com>
2017-10-30 05:56:03 +0900

    refactor: Split books into separate independent components.
    Instead of having all code in one page (i.e. App.js)
    we've used React's composition to separate into Web Components.

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
 
 class SearchBooks extends Component {
+  state = {
+    query: ""
+  };
+
+  handleQuery(query) {
+    this.setState({
+      query
+    });
+
+    BooksAPI.search(query).then(books => console.log(books));
+  }
+
   render() {
     return (
       <div className="search-books">
@@ -18,7 +31,13 @@ class SearchBooks extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={event => this.handleQuery(event.target.value)}
+              autoFocus
+            />
           </div>
         </div>
         <div className="search-books-results">
